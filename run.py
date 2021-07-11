a4663ce5fa556167518d65e1ecce71804d985734
Hussein-L-AlMadhachi <87253579+Hussein-L-AlMadhachi@users.noreply.github.com>
2021-07-11 07:37:33 +0300

    the luancher

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from sys import argv as arguements
+from os import system
+
+parameters = ""
+
+for arguement in arguements[ 1 : ]:
+    parameters += arguement
+
+system( "~/.Linins/linins.py " + parameters )
+
