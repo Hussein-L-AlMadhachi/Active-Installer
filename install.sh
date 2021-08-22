f1d45cc77cc1bc0e60182958e16c25359cb56c4c
Hussein Layth AlMadhachi <87253579+Hussein-L-AlMadhachi@users.noreply.github.com>
2021-08-22 13:17:11 +0300

    Add files via upload
    adding GNU General Public License version 2 headers for each script

--- install.sh
+++ install.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
+<<com
+
+Active source code installer
+Copyright (C) 2021  Hussein Layth Al-Madhachi
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License 
+as published by the Free Software Foundation, version 2
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+com
+
 echo Installing Active installer 2021 version 0.1.2
 
 echo "[o] Note: this requires root permission"
