e88701c6a691f6d8f0e3f308745e429dbfdc2bc6
Hussein Layth AlMadhachi <87253579+Hussein-L-AlMadhachi@users.noreply.github.com>
2023-11-27 22:37:07 +0300

    Update ParserLib.py
    a whole rewrite for the program including some breaking changes

--- ParserLib.py
+++ ParserLib.py
@@ -1,144 +1,179 @@
 #/usr/bin/env python3
 
 import os
+import json
 
-
-settings = {}
 
 
 """ in SettingFile class"""
 class SettingFile:
     def __init__( self ):
-        # this file stores the number that referes to the users distro and stores programs that needs root permissions
-        self.SettingFile = "/etc/active/SettingFile"
+        self.SettingFile = "/etc/active/SettingFile.json"
+        self.settings = {}
+        self.valid_os_ids = [ "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" ]
+        
+        if not os.path.exists( "/etc/active/SettingFile.json" ):
+            self.setup()
+        else:
+            self.load()
 
-    #   load settings from SettingFile    
     def load( self ):
         file_descriptor = open( self.SettingFile , "r" )
-
-        for line in file_descriptor:
-            feature , value = [   line[ : line.index( ":" ) ]   ,   line[ line.index( ":" ) + 1  :  -1  ]   ]
-            settings[ feature ] = value
+        self.settings = json.loads(file_descriptor.read())
         file_descriptor.close()
 
-    #   write changes to SettingFile
     def save( self ):
         try:
             file_descriptor = open( self.SettingFile , "w" )
         except PermissionError:
-            print( "[!] you must be root to do any changesto the settings" )
-        
-        for key in settings:
-            file_descriptor.write(  key   +  ":"  +  settings[ key ]  +  "\n"  )
+            print( "[!] you must be root to do any changes to the settings" )
+            exit(-1)
+        file_descriptor.write( json.dumps( self.settings ) )
         file_descriptor.close()
 
+    def setup( self ):
+        print( "current settings:\n" )
+        if self.settings != {}:
+            print( "\tos :" , self.settings["os"] )
+            choice = input( "\nDo you want to change the current options? [Y/N]  " )
+            if choice.strip().lower() != "y":
+                exit(0)
+        else:
+            print("setting active installer for the first time..\n\n")
+        
+        print( "What is the Linux disribution that you are using?" )
+        print( "\t1)  Arch Linux" )
+        print( "\t2)  Fedora Linux" )
+        print( "\t3)  OpenSUSE Linux" )
+        print( "\t4)  CentOS" )
+        print( "\t5)  Apline Linux" )
+        print( "\t6)  Gentoo Linux" )
+        print( "\t7)  BSD" )
+        print( "\t8)  Debian Linux" )
+        print( "\t9))  Ubuntu Linux" )
+        print( "\t10) Void Linux" )
+        
+        print( "\n\ttype 0 to exit" )
+
+        choice = str(input( "your choice: " ))
+
+        if choice.strip() in self.valid_os_ids:
+            self.settings["os"] = choice.strip()
+            self.save()
+        else:
+            print( "you have to choose on of the given numbers only" )
+    def info( self ):
+        print( self.settings )
 
 
-#   InstallFile
+
+from os import system
+
+
+selectors = ["pci" , "path" , "file"]
+
+
 class InstallFile:
-#   inintialisation
     def __init__( self ):
         file_descriptor = open( "/etc/active/HardwareInfo" , "r" )
         self.info = file_descriptor.read()
         file_descriptor.close()
 
 
-#   check wether a particular software available
-    def chack_hardware_device_availibility( self, device ):
+    def check_pci( self, device ):
         return  device in self.info
 
-
-#   check wether a particular software available
-    def check_software_availibility( self , path ):
+    def check_path( self , path ):
         return  os.path.exists( path )
 
 
-#   parse a line of script and 
-    def parse( self , line_of_script ):
-        ### useful fucntions
-        
-        self.line_of_script = line_of_script
-        #     remove all charaters that exist in an index
-        def remove_a_char_at( char , index ):
-            while char == self.line_of_script[ index ]:
-                self.line_of_script = self.line_of_script[ : index ] + self.line_of_script[  index + 1  :  ]
-        
-        # remove spaces from the beginning of the line
-        remove_a_char_at( " " , 0 )
+    def parse( self , scriptline ):
 
+        index = 0
+        
+        selector = ""
+        params = ""
+        should_execute = True
 
-        ### Parsing line_of_script
-        
-        #  don't parse emty lines
-        if len(  self.line_of_script.replace(" ","").replace("\t","").replace("\n","")  ) == 0:
-            return "#"
-        
-    #  distribution specific script
-        elif line_of_script[ 0 ] == settings[ "Distribution" ]:
-            # remove spaces at index [1]
-            remove_a_char_at ( " " , 1 )
-            self.script = self.line_of_script[ 1 : ]
-            return self.script
-    #  specify a group of distributions
-        elif   "{" == line_of_script[0]   and   "}" in line_of_script:
-            distro_group = (    line_of_script[  1  :  line_of_script.index( "}" )  ]    ).replace( " " , "" )
-            if "," in line_of_script:
-                if settings[ "Distribution" ] in distro_group.split(","):
-                    return line_of_script[  line_of_script.index( "}" ) + 1  :  ]
-                else:
-                    return "#"
-                
+        if ("{" in scriptline):
+            index = scriptline.index("{")
+            selector = scriptline[ :index ].strip()
+            if "not" in selector:
+                should_execute = False
+                selector = selector[:-3].strip()
+            
+            if not( selector in selectors):
+                return "Wrong selector: it should be one of the following  pci , file , cpuinfo , software"
+            
+            if "}" in scriptline[index:]:
+                params = scriptline[ index+1:scriptline.index("}") ]
+                return {"selector":selector , "should-execute":should_execute , "params":params}
             else:
-                print( "[!] Syntax Error: incorrect syntax in \n\t" + line_of_script )            
-    
-    #  hardware specific script
-        elif    "#" == self.line_of_script[ 0 ]:
-            if    ":" in self.line_of_script    and    self.chack_hardware_device_availibility(   self.line_of_script[  1  :  self.line_of_script.index(":")  ]   ):
-                remove_a_char_at(  " "  ,  self.line_of_script.index( ":" ) + 1  )
-                self.script = self.line_of_script[  line_of_script.index( ":" ) + 1  :  ]
-                return self.script
-            else:
-                return "#"
-        
-
-    #  software specific script
-        elif    "@"  ==  self.line_of_script[ 0 ]:
-            if    ":"  in  self.line_of_script[ 1 : ]    and    self.check_software_availibility(   self.line_of_script[  1  :  self.line_of_script.index(":")  ]   ):
-                remove_a_char_at(  " "  ,  self.line_of_script.index( ":" ) + 1  )
-                self.script = self.line_of_script[  self.line_of_script.index( ":" ) + 1  :  ]
-                return  self.script
-            else:
-                return "#"
-        
-        
-    #  general purpose script
-        elif not self.line_of_script[ 0 ].isnumeric():
-            remove_a_char_at ( " " , 0 )
-            self.script = self.line_of_script
-            return self.script
+                return "you cannot leave {} open"
+            
         else:
-            return "#"
-    
+            return "you need to add to put your parameters inside {}"
 
-    def debug( self , installfile ):
-        for  line  in  open( installfile ):
-            os.system("clear")
-            parsed = self.parse( line.replace( "\n" , "" ) )
-            print( "\n\n================================================================" )
-            print( ">>>>>>>>>>>>>>>>>>>> Installation process <<<<<<<<<<<<<<<<<<<<<<" )
-            print( "----------------------------------------------------------------" )
-            print(  "\nline of script = \""  +  line.replace( "\n" , "" )  +  "\""  )
-            print(  "parsed line of script = \""  +  parsed  +  "\"\n\n"  )
-            input( "           --- press enter to run the above script ---" )
-            print( "result:\n" )
-            os.system( parsed )
-            print( "\n----------------------------------------------------------------" )
-            print( ">>>>>>>>>>>>>>>>>> command has been executed <<<<<<<<<<<<<<<<<<" )
-            print( "================================================================" )
-            input( "\n\n             --- press enter to go to the next ---\n\n" )
+    def should_exec( self ,  parse_params ):
+        if parse_params["selector"] == "pci":
+            if parse_params["should-execute"]:
+                return self.check_pci( parse_params["params"] )
+            else:
+                return not self.check_pci( parse_params["params"] )
 
-    def execute( self , installfile ):
-        for  line  in  open( installfile ):
-            os.system(  self.parse( line )  )
+        elif parse_params["selector"] == "path":
+            if parse_params["should-execute"]: 
+                return self.check_path( parse_params["params"] )
+            else:
+                return not self.check_path( parse_params["params"] )
+        else:
+            print( "HOW THE HELL DO YOU GET HERE!!!" )
 
 
+
+    def execute( self, filename , debug=False ):
+        file_stream = open( filename , "r" )
+
+        script = ""
+        keep_going = False
+        should_execute = True
+        counter = 0
+        
+        for line in file_stream:
+            counter += 1
+            
+            if line.strip() == "":
+                continue
+
+            if keep_going:
+                if line.strip() == "end":
+                    if debug:
+                        if should_execute:
+                            print( "\033[32m" , counter , "|  end \033[0m\n" )
+                    keep_going = False
+
+                elif should_execute:
+                    if debug:
+                        print(  counter , " |\texecuting:" , line )
+                    system( line )
+            else:
+                p = self.parse( line )
+                if type(p) == str:
+                    print( counter , "|\t" , line )
+                    print( "ERROR in line " , counter , ":  " ,p )
+                    exit(-1)
+                else:
+                    should_execute = self.should_exec( p )
+                    keep_going = True
+                    if debug:
+                        if should_execute:
+                            if p["should-execute"]:
+                                print( "\033[32m", counter , " |   the " , p["selector"] , p["params"] ," exist, execute this {\033[0m" )
+                            else:
+                                print( "\033[32m", counter , " |   the " , p["selector"] , p["params"] ,"does not exist , execute this {\033[0m" )
+                        else:
+                            if p["should-execute"]:
+                                print( "\033[31m", counter , " |   the condition " , p["selector"] , p["params"] ," exist , was unmet\033[0m" )
+                            else:
+                                print( "\033[31m", counter , " |   the condition" , p["selector"] , p["params"] ,"does not exist , was unmet\033[0m" )
+        file_stream.close()
